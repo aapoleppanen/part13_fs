5743d934d144f9ae4335ec1e10cd4eedbd03f641
Aapo Leppänen <aapo.leppanen@gmail.com>
2021-12-22 14:57:06 +0200

    exercises 13.17-13.18

---
+++ migrations/20211223add_year.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+	up: async ({ context: queryInterface }) => {
+		await queryInterface.addColumn("blogs", "year", {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			hasYear(value) {
+				if (
+					parseInt(value) < 1991 ||
+					parseInt(value) > new Date().getFullYear
+				) {
+					throw new Error(
+						"Only blogs posted between 1991 and today are allowed."
+					);
+				}
+			},
+		});
+	},
+	down: async ({ context: queryInterface }) => {
+		await queryInterface.removeColumn("blogs", "year");
+	},
+};
